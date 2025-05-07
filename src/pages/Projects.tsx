e9db6bfcfafc11c8f050d33a083fb973219195df
mayagel <myagel2@gmail.com>
2025-05-07 16:59:59 +0300

    fiximages3?

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,10 +12,9 @@ type Project = {
   technologies: string[];
 };
 
-// Corrected glob paths - relative to 'src'
-const iotImages = import.meta.glob('./public/images/projects/iot/*.jpg', { eager: true });
-const schedulingImages = import.meta.glob('./public/images/projects/scheduling/*.jpg', { eager: true });
-const clashRoyaleImages = import.meta.glob('./public/images/projects/clash-royale/*.jpg', { eager: true });
+const iotImages = import.meta.glob('/public/images/projects/iot/*.jpg', { eager: true });
+const schedulingImages = import.meta.glob('/public/images/projects/scheduling/*.jpg', { eager: true });
+const clashRoyaleImages = import.meta.glob('/public/images/projects/clash-royale/*.jpg', { eager: true });
 
 
 
